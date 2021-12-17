068e8d2efd6813d6a2240a5b45fab82ad0ea0c53
Dawid Michalak <dawid.michalak0905@gmail.com>
2021-12-17 14:16:45 +0100

    Add parameters selection

--- src/main/java/pl/put/poznan/transformer/logic/JsonTransformer.java
+++ src/main/java/pl/put/poznan/transformer/logic/JsonTransformer.java
@@ -1,2 +1,45 @@
-package pl.put.poznan.transformer.logic;public class JsonTransformer {
+package pl.put.poznan.transformer.logic;
+
+import pl.put.poznan.transformer.logic.decorators.*;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class JsonTransformer {
+
+    private String[] transforms;
+    private String[] toSave;
+    private String[] toCut;
+
+    public JsonTransformer (String[] transforms, String[] toCut, String[] toSave) {
+        this.transforms = transforms;
+        this.toCut = toCut;
+        this.toSave = toSave;
+    }
+
+    public String transform(Json data) {
+
+        data = new JsonValidatorDecorator(data);
+
+        for (String transform : transforms) {
+            switch (transform) {
+                case "minify":
+                    data = new JsonMinifierDecorator(data);
+                    break;
+                case "clarify":
+                    data = new JsonClarifierDecorator(data);
+                    break;
+                case "cut":
+                    data = new JsonCutterDecorator(data, Arrays.asList(toCut));
+                    break;
+                case "save":
+                    data = new JsonSaverDecorator(data, Arrays.asList(toSave));
+                    break;
+                default:
+                    break; //throw
+            }
+        }
+        return data.getData();
+    }
+
 }
--- src/main/java/pl/put/poznan/transformer/rest/JsonToolsController.java
+++ src/main/java/pl/put/poznan/transformer/rest/JsonToolsController.java
@@ -3,6 +3,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 import pl.put.poznan.transformer.logic.*;
+import pl.put.poznan.transformer.logic.decorators.JsonMinifierDecorator;
+import pl.put.poznan.transformer.logic.decorators.JsonSaverDecorator;
 
 import java.util.Arrays;
 
@@ -13,34 +15,39 @@ public class JsonToolsController {
     private static final Logger logger = LoggerFactory.getLogger(JsonToolsController.class);
 
     @RequestMapping(method = RequestMethod.GET, produces = "application/json")
-    public String get(@PathVariable String text,
-                              @RequestParam(value="transforms", defaultValue="upper,escape") String[] transforms) {
+    public String get(@PathVariable String jsonRequest,
+                      @RequestParam(value="transforms", defaultValue="minify, cut") String[] transforms,
+                      @RequestParam(value="cut", defaultValue="") String[] toCut,
+                      @RequestParam(value="save", defaultValue="") String[] toSave) {
 
         // log the parameters
-        logger.debug(text);
+        logger.debug(jsonRequest);
         logger.debug(Arrays.toString(transforms));
+        logger.debug(Arrays.toString(toCut));
+        logger.debug(Arrays.toString(toSave));
 
-        Json json = new JsonMinifierDecorator ( new JsonData(text));
+        Json json = new JsonData(jsonRequest);
 
-        logger.debug(json.getData());
-
-        return json.getData();
+        JsonTransformer transformer = new JsonTransformer(transforms, toCut, toSave);
+        return transformer.transform(json);
     }
 
     @RequestMapping(method = RequestMethod.POST, produces = "application/json")
-    public String post(@PathVariable String text,
-                      @RequestBody String jsonRequest) {
+    public String post(@RequestParam(value="transforms", defaultValue="minify, cut") String[] transforms,
+                       @RequestParam(value="cut", defaultValue="") String[] toCut,
+                       @RequestParam(value="save", defaultValue="") String[] toSave,
+                       @RequestBody String jsonRequest) {
 
-        Json json1 = new JsonData(jsonRequest);
-//        Json json2 = new JsonValidatorDecorator(json1);
-//        if(json2.getData().equals("Invalid Json"))
-//            return "Invalid Json";
-//        else
-//            return json2.getData();
+        // log the parameters
+        logger.debug(jsonRequest);
+        logger.debug(Arrays.toString(transforms));
+        logger.debug(Arrays.toString(toCut));
+        logger.debug(Arrays.toString(toSave));
 
-        Json json = new JsonClarifierDecorator (json1);
-        logger.debug(json.getData());
-        return json.getData();
+        Json json = new JsonData(jsonRequest);
+
+        JsonTransformer transformer = new JsonTransformer(transforms, toCut, toSave);
+        return transformer.transform(json);
     }
 }
 
