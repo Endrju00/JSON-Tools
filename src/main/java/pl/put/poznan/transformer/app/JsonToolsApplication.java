0498d7a50b90f25ce259205ee52723dd3f404576
Andrzej Kapczyński <75850182+Endrju00@users.noreply.github.com>
2021-12-19 15:58:35 +0100

    Delete test documentation

--- src/main/java/pl/put/poznan/transformer/app/JsonToolsApplication.java
+++ src/main/java/pl/put/poznan/transformer/app/JsonToolsApplication.java
@@ -3,11 +3,6 @@ package pl.put.poznan.transformer.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * Class description
- * @author Me and others
- * @version 1.1
- */
 @SpringBootApplication(scanBasePackages = {"pl.put.poznan.transformer.rest", "pl.put.poznan.transformer.exceptions"})
 public class JsonToolsApplication {
 
